2c1a15b35c9306d89ed947091636b85994237500
Emil Gardström <emil.gardstrom@gmail.com>
2023-01-09 05:57:43 +0100

    only rerun on build.rs change

--- build.rs
+++ build.rs
@@ -2,4 +2,5 @@ fn main() {
     let version = unsafe { libc::gnu_get_libc_version() };
     let str = unsafe { std::ffi::CStr::from_ptr(version) };
     println!("cargo:warning=libc: {:?}", str);
+    println!("cargo:rerun-if-changed=build.rs");
 }
