d1a58a8c7b72f9f6f6a7e272d6af0ff6ac98d0e7
Emil Gardström <emil.gardstrom@gmail.com>
2023-01-09 05:47:33 +0100

    nice

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
     let version = unsafe { libc::gnu_get_libc_version() };
     let str = unsafe { std::ffi::CStr::from_ptr(version) };
-    println!("libc: {:?}", str);
+    println!("cargo:warning=libc: {:?}", str);
 }
